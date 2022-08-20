db6f85a5ff5bbfcc817daab4b6dbbe6866b001c2
Stefan Ihle <stefanihle@gmx.net>
2022-08-20 17:27:33 +0200

    create rtkq slice and config store and fetch pokemon from api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { useGetPokemonByNameQuery } from './services/pokemon';
+
 const App = () => {
-	return <div>Cool!</div>;
+	const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur');
+
+	return (
+		<div className='App'>
+			{error ? (
+				<>Oh no, there was an error</>
+			) : isLoading ? (
+				<>Loading...</>
+			) : data ? (
+				<>
+					<h3>{data.species.name}</h3>
+					<img src={data.sprites.front_shiny} alt={data.species.name} />
+				</>
+			) : null}
+		</div>
+	);
 };
 
 export default App;
